2fceb744beb28cb4bd0ec7f089539fd34efc5939
pawansubedi <pawansubedi90558@gmail.com>
2024-05-16 11:50:08 +0545

    footer second half

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return (
         <div className="py-5 text-center">
             <p className="text-sm mt-2 opacity-50">
-                &copy; new Date().getFullYear() Pawan Subedi. All Rights Reserved.
+                &copy; {new Date().getFullYear()} Pawan Subedi. All Rights Reserved.
             </p>
 
         </div>
