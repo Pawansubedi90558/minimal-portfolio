78bdeb6ec3ddd61af745b5002d204f1197fd69c1
pawansubedi <pawansubedi90558@gmail.com>
2024-05-16 21:02:26 +0545

    dark mode on

--- src/App.jsx
+++ src/App.jsx
@@ -70,13 +70,15 @@ function App() {
 		>
 			{theme === 'dark' ? sun : moon}
 		</button>
-     <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter max-w-5xl w-11/12 mx-auto">
-      <Intro />
-      <Portfolio />
-      <Timeline />
-      <Contact />
-      <Footer />
+     <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+      <div className="max-w-5xl w-11/12 mx-auto">
+        <Intro />
+        <Portfolio />
+        <Timeline />
+        <Contact />
+        <Footer />
       </div>
+    </div>
     </>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro(){
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Pawan Subedi</h1>
+            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Pawan Subedi</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Web and Application developer</p>
             <p className="text-sm max-w-xl mb-6 font-bold">I am a full stack web developer based on Nepal currently pursuing B.Sc.CSIT degree on TU, IOST, mostly interested on developing projects which facilitate me and users make life at ease than before.<br />My other interests include studying philosophy, quantum physics and strange people and places.I write my blogs over <a
              href="https://www.blogger.com/blog/posts/8841954045602804882?hl=en&tab=jj"
