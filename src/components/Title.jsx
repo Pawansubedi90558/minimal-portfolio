421a9f3763d2dc62cabb78b7ade7d5b53c09c389
pawansubedi <pawansubedi90558@gmail.com>
2024-05-16 11:58:16 +0545

    Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+function Title({children, id}){
+    return (
+    <h1
+        id={id && id}
+        className="text-2xl font-bold underline underline-offset-8 decoration-4 mb-5 text-store-900"
+    >
+        {children}
+    </h1>
+    )
+}
+export default Title;
