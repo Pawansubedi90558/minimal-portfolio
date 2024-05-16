69b0c9e7cdf23e415aa2b97fccb889cd70616d7c
pawansubedi <pawansubedi90558@gmail.com>
2024-05-16 11:47:38 +0545

    footer half

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function Footer(){
     return (
-        <div></div>
+        <div className="py-5 text-center">
+            <p className="text-sm mt-2 opacity-50">
+                &copy; new Date().getFullYear() Pawan Subedi. All Rights Reserved.
+            </p>
+
+        </div>
     )
 }
 export default Footer;
