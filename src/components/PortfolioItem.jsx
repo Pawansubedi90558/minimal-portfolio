e4711f51521883e488da63b05d63d48260b3a469
pawansubedi <pawansubedi90558@gmail.com>
2024-05-16 21:35:48 +0545

    dark portfolio

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,18 +2,18 @@ function PortfolioItem({title,imgUrl,alt,stack,link}){
     return(
         <a 
             href={link}
-            target="_blank"
+            target="_self"
             rel="noreferrer noopener"
-            className="border-2 border-stone-900 rounded-md overflow-hidden"
+            className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
         >
             <img src={imgUrl}
                  alt={alt}
                  className="w-full h-36 md:h-48 object-cover cursor-pointer" />
             <div className="w-full p-4">
-                 <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
-                 <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                 <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">{title}</h3>
+                 <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
                     {stack.map(item =>(
-                        <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md">{item}</span>
+                        <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">{item}</span>
                     ))}
                  </p>
             </div>
