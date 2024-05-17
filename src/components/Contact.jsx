7e73373a56a6574b92397f84ca2414c723c69cb4
pawansubedi <pawansubedi90558@gmail.com>
2024-05-17 15:55:37 +0545

    timeline reversed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import Title from "./Title";
 
 function Contact(){
+    const validateForm = (e) => {
+        e.preventDefault();
+
+        const name = e.target.elements.name.value;
+        const email = e.target.elements.email.value;
+        const message = e.target.elements.message.value;
+
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/ ;
+        if(!emailPattern.test(email)){
+            alert("Please enter a valid email address");
+            return false;
+        }
+        // Check if name or message fields are empty or contain only whitespace
+        if(name.trim() === "" || message.trim() === ""){
+            alert("Please fill in all fields");
+            return false;
+        }
+        e.target.submit();
+        return true;
+    }
+
     return (
         <div className="flex flex-col mb-10 mx-auto">
             <div className="flex justify-center items-center">
@@ -26,6 +47,7 @@ function Contact(){
                               className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                    />
                    <button type="submit"
+                           onSubmit={validateForm}
                             // className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                             className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white dark:text-white"
                             >Contact Me</button>
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,15 +1,9 @@
 export default [
     {
-        year: '2019-2021',
-        title : 'School Leaving Certificate',
-        duration : '2 years',
-        details :"studied at Nidi Secondary School under NEB taking up science."
-    },
-    {
-        year: '2021-to date',
-        title : 'B.Sc.CSIT degree',
-        duration : '4 years',
-        details :"studying at MMC under TU taking up B.Sc.CSIT."
+        year: '2022-to date',
+        title : 'Software developer',
+        duration : '2 years+',
+        details :"providing web and mobile services as a software developer for clients whilst building my own stuffs."
     },
     {
         year: '2022-2023',
@@ -18,9 +12,15 @@ export default [
         details :"provided online training on python programming language under an institution based on Bangalore."
     },
     {
-        year: '2022-to date',
-        title : 'Software developer',
-        duration : '2 years+',
-        details :"providing web and mobile services as a software developer for clients whilst building my own stuffs."
+        year: '2021-to date',
+        title : 'B.Sc.CSIT degree',
+        duration : '4 years',
+        details :"studying at MMC under TU taking up B.Sc.CSIT."
+    },
+    {
+        year: '2019-2021',
+        title : 'School Leaving Certificate',
+        duration : '2 years',
+        details :"studied at Nidi Secondary School under NEB taking up science."
     }
 ]
